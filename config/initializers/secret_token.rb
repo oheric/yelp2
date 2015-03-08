f8dc9fbcc15a69a8477a50f2c1fd42c5780f653d
oheric <ohericyoung@gmail.com>
2015-03-07 20:34:26 -0800

    Initialize Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yelp2::Application.config.secret_key_base = 'fa54fc674be060e05beedb9aa310d0791bf2fb8f30bad9a0d7084236b7a6d40277ed1dbb80189dbec24b27575a98356e6e9024a41c933abcbcf71572a3d0a534'
